39aa35d80ffc805140a770004edd6fffc8aa093c
apple <apple@sunhaosheng.local>
2015-09-22 20:05:01 +0800

    Initial Commit

---
+++ SHSIM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHSIM
+//
+//  Created by apple on 15/9/22.
+//  Copyright (c) 2015年 SHSDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHSIM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHSIM
+//
+//  Created by apple on 15/9/22.
+//  Copyright (c) 2015年 SHSDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
